33a1140f9c6434bbc9a8073b9d93f4e3338f32d4
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-02-21 14:43:25 +0000

    Adding half-arrows for first order correction
    Former-commit-id: e532a78593ae007e326d2b5673d4e57e1d2a6d0d

--- shear_measurement_correction/plot_calibration_scatters.py
+++ shear_measurement_correction/plot_calibration_scatters.py
@@ -196,9 +196,16 @@ def main(argv):
                                 marker='x',s=markersize,label=label_1p,color=color)
                 
                     # Draw lines connecting points for successive calibrations
-                    ax.annotate("",xytext=(res["cp_sigma"],res["mp_sigma"]),xy=(res["cpp_sigma"],res["mpp_sigma"]),arrowprops=dict(arrowstyle="->",color=color))
+                    ax.annotate("",xytext=(res["cp_sigma"],res["mp_sigma"]),xy=(res["cpp_sigma"],res["mpp_sigma"]),
+                                arrowprops=dict(arrowstyle="->",color=color))
                     
-                ax.annotate("",xytext=(res["cm_sigma"],res["mm_sigma"]),xy=(res["cp_sigma"],res["mp_sigma"]),arrowprops=dict(arrowstyle="->",color=color))
+                # Also draw half-arrows for the first correction
+                ax.annotate("",xytext=(res["cm_sigma"],res["mm_sigma"]),
+                            xy=((res["cm_sigma"]+res["cp_sigma"])/2,(res["mm_sigma"]+res["mp_sigma"])/2),
+                            arrowprops=dict(arrowstyle="->",color=color))
+                ax.annotate("",xytext=((1.15*res["cm_sigma"]+res["cp_sigma"])/2.15,(1.15*res["mm_sigma"]+res["mp_sigma"])/2.15),
+                            xy=(res["cp_sigma"],res["mp_sigma"]),
+                            arrowprops=dict(arrowstyle="->",color=color))
                 
         ax.legend(loc='lower right',scatterpoints=1)
     
